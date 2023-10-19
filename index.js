1838afaa1746e7c09358e418e41f1e8da93ff43d
ManariaFI <146720736+ManariaFI@users.noreply.github.com>
2023-10-19 14:46:25 +0300

    Beta build
    Added starting screen, changed algorithm of how data is organized for simpler bug fixing. Fixed the ACE bug. Releasing the application officially!

--- index.js
+++ index.js
@@ -1,212 +1,286 @@
-let playerName = "Mana"
-let chips = 200
+//Player and dealer data
+let player = {
+    name: "Player",
+    chips: 200,
+    hand: [],
+    sum: 0,
+    double: false,
+    canHit: false,
+    aceCount: 0,
+    firstCard: "",
+    secondCard: "",
+};
+let dealer = {
+    chips: 200,
+    hand: [],
+    sum: 0,
+    aceCount: 0,
+    firstCard: "",
+    secondCard: "",
+};
+let message = "";
 
+// Elements for interface
+const messageEl = document.getElementById("message-el");
+const sumEl = document.getElementById("sum-el");
+const cardsEl = document.getElementById("cards-el");
+const playerEl = document.getElementById("player-el");
+const dealerSumEl = document.getElementById("dealer-sum-el");
+const dealerCardsEl = document.getElementById("dealer-cards-el");
+const enterBtnEl = document.getElementById("enterBtn");
+const dealerEl = document.getElementById("dealer-el");
+const nameEl = document.getElementById("name");
 
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let isDouble = false
-let message = ""
-const messageEl = document.getElementById("message-el")
-const sumEl = document.getElementById("sum-el")
-const cardsEl = document.getElementById("cards-el")
-const playerEl = document.getElementById("player-el")
-const dealerSumEl = document.getElementById("dealer-sum-el")
-const dealerCardsEl = document.getElementById("dealer-cards-el")
+playerEl.textContent = player.name + ": $" + player.chips;
 
-playerEl.textContent = playerName + ": $" + chips
-
-let dealerCards = []
-let dealerSum = 0
-let dealerTurn = false
-
-
-bootUp()
-function bootUp(){
-    doubleBtn.style.display = "none"
-    splitBtn.style.display = "none"
-    newCardBtn.style.display = "none"
-    standBtn.style.display = "none"
-    dealerBtn.style.display = "none"
-
+function renderData() {
+    playerEl.textContent = player.name + ": $" + player.chips;
+    dealerEl.textContent = "The House: $" + dealer.chips;
 }
 
-function mainMenu(){
-        doubleBtn.style.display = "none"
-        newCardBtn.style.display = "none"
-        standBtn.style.display = "none"
-        dealerBtn.style.display = "none"
-        startBtn.style.display = "initial"
-        isDouble = false
-}
+//When game is started:
+hideButtons();
+startBtn.style.display = "none";
 
+enterBtnEl.addEventListener("click", function () {
+    player.name = nameEl.value;
+    enterBtn.style.display = "none";
+    nameEl.style.display = "none";
+    yourName.style.display = "none";
+    mainMenu();
+    renderData();
+});
+//Hides all buttons from interface
+function hideButtons() {
+    doubleBtn.style.display = "none";
+    splitBtn.style.display = "none";
+    newCardBtn.style.display = "none";
+    standBtn.style.display = "none";
+    dealerBtn.style.display = "none";
+}
+function fullReset() {
+    //resets all the flags for the next round
+    player.hasBlackJack = false;
+    player.aceCount = 0;
+    player.double = false;
+    player.canHit = false;
+    dealer.hasBlackJack = false;
+    dealer.aceCount = 0;
+    dealer.canHit = false;
+}
+function mainMenu() {
+    hideButtons();
+    fullReset();
+    startBtn.style.display = "initial";
+}
+//Gameplay related functions
 function getRandomCard() {
-    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
     if (randomNumber > 10) {
-        return 10
+        return 10;
     } else if (randomNumber === 1) {
-        return 11
+        if (player.canHit === true) {
+            player.aceCount += 1;
+            return 11;
+        } else {
+            dealer.aceCount += 1;
+            return 11;
+        }
     } else {
-        return randomNumber
+        return randomNumber;
     }
 }
-function bet(){
-    chips -= 10
-    console.log(chips)
-    playerEl.textContent = playerName + ": $" + chips
-}
-function blackJackPay(){
-    chips += 25
-    console.log(chips)
-    playerEl.textContent = playerName + ": $" + chips
-}
-function winPay(){
-    chips += 20
-    console.log(chips)
-    playerEl.textContent = playerName + ": $" + chips
-}
-function pushPay(){
-    chips += 10
-    console.log(chips)
-    playerEl.textContent = playerName + ": $" + chips
-}
 
+function newCard() {
+    console.log(getRandomCard);
+    if (player.canHit === true) {
+        let card = getRandomCard();
+        player.sum += card;
+        player.hand.push(card);
+        doubleBtn.style.display = "none";
+        renderGame();
+    }
+}
+function dealerCard() {
+    let card = getRandomCard();
+    dealer.sum += card;
+    dealer.hand.push(card);
+    renderDealer();
+}
+//Checks if hand have ace with value of 11, if they do and would bust substract 10 points from score
+
+function aceCheck() {
+    for (let i = 0; i <= player.aceCount; i++) {
+        if (player.sum > 21 && player.aceCount >= 1) {
+            player.aceCount -= 1;
+            player.sum -= 10;
+        }
+    }
+}
+function dealerAceCheck() {
+    for (let i = 0; i <= dealer.aceCount; i++) {
+        if (dealer.sum > 21 && dealer.aceCount >= 1) {
+            dealer.aceCount -= 1;
+            dealer.sum -= 10;
+        }
+    }
+}
+//
+//Bet payout functions
+
+function bet() {
+    player.chips -= 10;
+    dealer.chips += 10;
+
+    renderData();
+}
+function blackJackPay() {
+    player.chips += 25;
+    dealer.chips -= 25;
+    renderData();
+}
+function winPay() {
+    player.chips += 20;
+    dealer.chips -= 20;
+    renderData();
+}
+function pushPay() {
+    chips += 10;
+    dealer.chips -= 10;
+    renderData();
+}
+//
 function startGame() {
-    bet()
-    doubleBtn.style.display = "initial"
-    newCardBtn.style.display = "initial"
-    standBtn.style.display = "initial"
-    startBtn.style.display = "none"
-    hasBlackJack = false
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    let dFirstCard = getRandomCard()
-    dealerCards = [dFirstCard]
-    dealerSum = dFirstCard
-    renderGame()
-  
+    bet();
+    doubleBtn.style.display = "initial";
+    newCardBtn.style.display = "initial";
+    standBtn.style.display = "initial";
+    startBtn.style.display = "none";
+    hasBlackJack = false;
+    player.canHit = true;
+    player.firstCard = getRandomCard();
+    player.secondCard = getRandomCard();
+    player.hand = [player.firstCard, player.secondCard];
+    player.sum = player.firstCard + player.secondCard;
+    dealer.firstCard = getRandomCard();
+    dealer.secondCard = getRandomCard();
+    dealer.hand = [dealer.firstCard, "??"];
+    dealer.sum = dealer.firstCard;
+    renderGame();
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    aceCheck();
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < player.hand.length; i++) {
+        cardsEl.textContent += player.hand[i] + " ";
     }
-    
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-            message = "Do you want to draw a new card?"
-
-    } else if (sum === 21 && cards.length===2) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-        blackJackPay()
-        mainMenu()
-    } else if (sum === 21) {
-        message = "You have 21!"
-        hasBlackJack = true
-        bootUp()
-        dealerBtn.style.display = "initial"
-
-
-    } else{
-        message = "You're out of the game!"
-        isAlive = false
-        mainMenu()
+    sumEl.textContent = "Sum: " + player.sum;
+    dealerCardsEl.textContent = "Dealer's cards: ";
+    for (let i = 0; i < dealer.hand.length; i++) {
+        dealerCardsEl.textContent += dealer.hand[i] + " ";
     }
-    messageEl.textContent = message
-
-    dealerCardsEl.textContent = "Dealer's cards: "
-    for (let i = 0; i < dealerCards.length; i++){
-        dealerCardsEl.textContent += dealerCards[i] + " "
+    dealerSumEl.textContent = "Dealer's sum: " + dealer.sum;
+    if (player.sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (player.sum === 21 && player.hand.length === 2) {
+        message = "You've got Blackjack!";
+        blackJackPay();
+        mainMenu();
+    } else if (player.sum === 21) {
+        message = "You have 21!";
+        hideButtons();
+        player.canHit = false;
+        dealerBtn.style.display = "initial";
+    } else {
+        message = "You're out of the game!";
+        player.canHit = false;
+        mainMenu();
     }
-    dealerSumEl.textContent = "Dealer's sum: " + dealerSum
+    messageEl.textContent = message;
 }
 function double() {
-    isDouble = true
-    bet()
-    let card = getRandomCard()
-        sum += card
-        cards.push(card)
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+    player.double = true;
+    bet();
+    let card = getRandomCard();
+    player.sum += card;
+    player.hand.push(card);
+    aceCheck();
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < player.hand.length; i++) {
+        cardsEl.textContent += player.hand[i] + " ";
     }
-    
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "DOUBLE DOWN!!!"
-        bootUp()
-        dealerBtn.style.display = "initial"
-
-    } else if (sum === 21) {
-        message = "YOU HAVE TWENTY-ONE!!!"
-        bootUp()
-        dealerBtn.style.display = "initial"
-
-
+    dealerCardsEl.textContent = "Dealer's cards: ";
+    for (let i = 0; i < dealer.hand.length; i++) {
+        dealerCardsEl.textContent += dealer.hand[i] + " ";
+    }
+    dealerSumEl.textContent = "Dealer's sum: " + dealer.sum;
+    sumEl.textContent = "Sum: " + player.sum;
+    dealer.hand = [dealer.firstCard, dealer.secondCard];
+    dealer.sum = dealer.firstCard + dealer.secondCard;
+    if (player.sum <= 20) {
+        message = "DOUBLE DOWN!!!";
+        hideButtons();
+        player.canHit = false;
+        dealerBtn.style.display = "initial";
+    } else if (player.sum === 21) {
+        message = "YOU HAVE TWENTY-ONE!!!";
+        hideButtons();
+        player.canHit = false;
+        dealerBtn.style.display = "initial";
     } else {
-        message = "OUT!!!!! YEEHAW!"
-        isAlive = false
-        mainMenu()
+        message = "OUT!!!!! YEEHAW!";
+        player.canHit = false;
+        mainMenu();
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
-
-function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        doubleBtn.style.display = "none"
-        renderGame()
-        
+function stand() {
+    hideButtons();
+    message = "Ok, it's my turn now!";
+    dealerBtn.style.display = "initial";
+    player.canHit = false;
+    dealer.hand = [dealer.firstCard, dealer.secondCard];
+    dealer.sum = dealer.firstCard + dealer.secondCard;
+    dealerCardsEl.textContent = "Dealer's cards: ";
+    for (let i = 0; i < dealer.hand.length; i++) {
+        dealerCardsEl.textContent += dealer.hand[i] + " ";
     }
-}
-function stand(){
-     bootUp()
-     dealerBtn.style.display = "initial"
+    dealerSumEl.textContent = "Dealer's sum: " + dealer.sum;
+    messageEl.textContent = message;
 }
 function renderDealer() {
-
-    dealerCardsEl.textContent = "Dealer's cards: "
-    for (let i = 0; i < dealerCards.length; i++){
-        dealerCardsEl.textContent += dealerCards[i] + " "
+    dealerAceCheck();
+    dealerCardsEl.textContent = "Dealer's cards: ";
+    for (let i = 0; i < dealer.hand.length; i++) {
+        dealerCardsEl.textContent += dealer.hand[i] + " ";
     }
-    dealerSumEl.textContent = "Dealer's sum: " + dealerSum
+    dealerSumEl.textContent = "Dealer's sum: " + dealer.sum;
 
-    if (dealerSum > 16 && sum < dealerSum && dealerSum < 22) {
-        message = "The house wins!"
-        mainMenu()
-
-    } else if (dealerSum > 16 && sum === dealerSum && dealerSum < 22) {
-        message = "PUSH! Returning your bet!"
-        if (isDouble === true){
-            pushPay()
+    if (dealer.sum > 16 && player.sum < dealer.sum && dealer.sum < 22) {
+        message = "The house wins!";
+        mainMenu();
+    } else if (
+        dealer.sum > 16 &&
+        player.sum === dealer.sum &&
+        dealer.sum < 22
+    ) {
+        message = "PUSH! Returning your bet!";
+        if (player.double === true) {
+            pushPay();
         }
-        pushPay()
-        mainMenu()
-
-    } else if (dealerSum < 17){
-        message = "Drawing, as per rules."
+        pushPay();
+        mainMenu();
+    } else if (dealer.sum < 17) {
+        message = "Drawing, as per rules.";
     } else {
-        message = "You won this round!"
-        if (isDouble === true){
-            winPay()
+        message = "You won this round!";
+        if (player.double === true) {
+            winPay();
         }
-        winPay()
-        mainMenu()
+        winPay();
+        mainMenu();
     }
-    messageEl.textContent = message
-}
-function dealerNewCard(){
-    let card = getRandomCard()
-        dealerSum += card
-        dealerCards.push(card)
-        renderDealer()
+    messageEl.textContent = message;
 }
